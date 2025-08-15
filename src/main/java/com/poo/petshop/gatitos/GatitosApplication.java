5ae17df64b6eef9d497a3a490e8355e6e51fa9bd
Usuário <Guilherme Santana>
2025-08-14 23:57:48 -0300

    First commit

---
+++ src/main/java/com/poo/petshop/gatitos/GatitosApplication.java
@@ -0,0 +1,13 @@
+package com.poo.petshop.gatitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatitosApplication.class, args);
+	}
+
+}
